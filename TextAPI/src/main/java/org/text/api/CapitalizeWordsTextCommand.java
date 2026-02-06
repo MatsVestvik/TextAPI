14577c9b56b5a9c2b29240d872ccd231e8e92026
MatsVestvik <mats.o.vestvik@gmail.com>
2026-02-06 18:38:41 +0100

    all the capitalize classes complete with one test for each

--- TextAPI/src/main/java/org/text/api/CapitalizeWordsTextCommand.java
+++ TextAPI/src/main/java/org/text/api/CapitalizeWordsTextCommand.java
@@ -4,6 +4,13 @@ public class CapitalizeWordsTextCommand extends CapitalizeTextCommand {
 
   @Override
   public String execute(String text) {
-    return text.toUpperCase();
+    StringBuilder sb = new StringBuilder();
+    String[] words = text.split(" ");
+
+    for (String word : words) {
+      word = word.substring(0, 1).toUpperCase() + word.substring(1);
+      sb.append(word + " ");
+    }
+    return sb.toString().trim();
   }
 }
--- TextAPI/src/test/java/org/text/api/CapitalizeWordsTextCommandTest.java
+++ TextAPI/src/test/java/org/text/api/CapitalizeWordsTextCommandTest.java
@@ -6,19 +6,19 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class CapitalizeTextCommandTest {
+public class CapitalizeWordsTextCommandTest {
 
-  private CapitalizeTextCommand capitalizeTextCommand;
+  private CapitalizeWordsTextCommand capitalizeWordsTextCommand;
 
   @BeforeEach
   void setup() {
-    capitalizeTextCommand = new CapitalizeTextCommand();
+    capitalizeWordsTextCommand = new CapitalizeWordsTextCommand();
   }
 
   @Test
   @DisplayName("positive")
   void testCapitalization() {
-    String result = capitalizeTextCommand.execute("this is a text");
-    assertEquals("This is a text", result);
+    String result = capitalizeWordsTextCommand.execute("this is a text");
+    assertEquals("This Is A Text", result);
   }
 }
